88248357df4ca13a76906ad87ee8bc0c8bdc7a17
“Rockstar100” <parveenjaiswal100@gmail.com>
2025-08-01 16:11:38 +0530

    Remove instructional sections from CreateUserPage and UserManagementPage components to streamline user interface.

--- src/app/create-user/page.jsx
+++ src/app/create-user/page.jsx
@@ -18,41 +18,7 @@ const CreateUserPage = () => {
             </div>
           </div>
 
-          {/* Instructions */}
-          <div className="row mt-4">
-            <div className="col-12">
-              <div className="card">
-                <div className="card-header">
-                  <h5 className="card-title">
-                    <Icon icon="mdi:information" className="me-2" />
-                    User Creation Guide
-                  </h5>
-                </div>
-                <div className="card-body">
-                  <div className="row">
-                    <div className="col-md-6">
-                      <h6>📧 Email Setup:</h6>
-                      <ul>
-                        <li>Enter the user's email address</li>
-                        <li>User will receive a password reset email</li>
-                        <li>They can set their password and sign in</li>
-                      </ul>
-                    </div>
-                    <div className="col-md-6">
-                      <h6>🔐 Role Assignment:</h6>
-                      <ul>
-                        <li><strong>No Access</strong> - User cannot access anything</li>
-                        <li><strong>User</strong> - Basic access to assigned features</li>
-                        <li><strong>Manager</strong> - Team management and analytics</li>
-                        <li><strong>Admin</strong> - User management and system access</li>
-                        <li><strong>Super Admin</strong> - Full system control</li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+         
 
         </MasterLayout>
       </RoleGuard>
--- src/app/user-management/page.jsx
+++ src/app/user-management/page.jsx
@@ -22,41 +22,7 @@ const UserManagementPage = () => {
             </div>
           </div>
 
-          {/* Instructions */}
-          <div className="row mt-4">
-            <div className="col-12">
-              <div className="card">
-                <div className="card-header">
-                  <h5 className="card-title">
-                    <Icon icon="mdi:information" className="me-2" />
-                    How to Manage User Roles
-                  </h5>
-                </div>
-                <div className="card-body">
-                  <div className="row">
-                    <div className="col-md-6">
-                      <h6>📋 Steps to Assign Roles:</h6>
-                      <ol>
-                        <li>Users must first sign up/sign in to your application</li>
-                        <li>Their UID will be automatically created in Firebase Auth</li>
-                        <li>Use this interface to assign roles to their UID</li>
-                        <li>Roles will be stored in Firestore under the 'users' collection</li>
-                      </ol>
-                    </div>
-                    <div className="col-md-6">
-                      <h6>🔐 Role Hierarchy:</h6>
-                      <ul>
-                        <li><strong>Super Admin</strong> - Full system access, can manage roles</li>
-                        <li><strong>Admin</strong> - Administrative access, can manage users</li>
-                        <li><strong>Manager</strong> - Management access, can view analytics</li>
-                        <li><strong>User</strong> - Basic access, personal dashboard</li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+         
 
         </MasterLayout>
       </RoleGuard>
