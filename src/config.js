1319bdaf458ffa41afeedc1072ddb555a403ee66
NishantPriyadarshi063 <nishant@seleric.ai>
2025-09-30 10:49:29 +0530

    fix: update API baseURL to use Azure endpoint and comment out localhost option

--- src/config.js
+++ src/config.js
@@ -2,9 +2,9 @@
 
 const config = {
   api: {
-    baseURL: process.env.NODE_ENV === 'production' 
-      ? "https://dashbackend-a3cbagbzg0hydhen.centralindia-01.azurewebsites.net"
-      : "http://localhost:8080",
+    baseURL:
+      "https://dashbackend-a3cbagbzg0hydhen.centralindia-01.azurewebsites.net",
+    // "http://localhost:8080",
   },
 };
 
