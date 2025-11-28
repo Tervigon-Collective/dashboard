9ac39ee906de2dad27eeef04fd26d5ccfc60162c
NishantPriyadarshi063 <nishant@seleric.ai>
2025-11-28 13:29:19 +0530

    Update DEFAULT_API_BASE_URL to new production endpoint

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@
 const DEFAULT_API_BASE_URL =
   //"https://dashbackend-a3cbagbzg0hydhen.centralindia-01.azurewebsites.net";
   // "http://localhost:8080";
-  "http://72.61.228.168:8081";
+  "https://node.seleric.cloud";
 const DEFAULT_PYTHON_API_BASE_URL =
   "https://contentgeneratorbackend-gvcpgcd6enavdag9.centralindia-01.azurewebsites.net";
 // "http://localhost:8000";
